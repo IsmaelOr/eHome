65ae1bffcbb709a2b0b78fa32fcc00195d78ba90
Ismael Ortega Estrada <ismaelortega80@gmail.com>
2023-05-12 23:48:45 -0600

    Cancel Reservations as owner it's working

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -82,7 +82,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                             label="My properties"/>                        
 
                             <MenuItem 
-                            onClick={() => {}}
+                            onClick={() => router.push("/reservations")}
                             label="My reservations"/>
                             
                             <MenuItem 
